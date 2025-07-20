b49b5dceb9e0dc971d63b6ca5e0869687c629f45
kadhir-1984 <kadhir1084@gmail.com>
2025-07-20 12:56:30 +0530

    updated login page and created css for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './login.css'; 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Future: Send to backend
+    console.log('Login Submitted', { email, password });
+  };
+
   return (
-    <div className="page">
-      <h2 className="page-title">Login</h2>
-      <p>This is the login page.</p>
+    <div className="page small">
+      <h2 className="login-title">Login to Your Account</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <p className="redirect">
+        Don’t have an account? <Link to="/register">Register</Link>
+      </p>
     </div>
   );
 };
